3f24d127ad80d502cb5e84ce6059ac1808b2f665
fyui001 <yun.cod@gmail.com>
2021-11-07 23:14:11 +0900

    create migration files

---
+++ database/migrations/2021_11_07_132546_matsui_eriko_tweets.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Database\Libs\BlueprintTrait;
+
+class MatsuiErikoTweets extends Migration
+{
+    use BlueprintTrait;
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('matsui_eriko_tweets', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('account_name')->comment('アカウント名');
+            $table->string('tweet_id')->unique('tweet_id_unique')->comment('ツイートID');
+            $table->string('tweet_url')->comment('ツイートURL');
+            $this->dateTimes($table);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('matsui_eriko_tweets');
+    }
+}
